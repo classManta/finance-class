363277d3349ac5599bdc8b42d9665881601eadaf
PlayerManta <nimbleMantaray@gmail.com>
2026-03-19 01:37:05 +0800

    week4 change # this is so dead

---
+++ week4/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace COMSample1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1LoginID = new System.Windows.Forms.TextBox();
+            this.textBox2Password = new System.Windows.Forms.TextBox();
+            this.button1Login = new System.Windows.Forms.Button();
+            this.buttonEnterMonitorLONG_click = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.StockList = new System.Windows.Forms.ListBox();
+            this.gvListData = new System.Windows.Forms.DataGridView();
+            this.button1gvSearch = new System.Windows.Forms.Button();
+            this.textBox1StockData = new System.Windows.Forms.TextBox();
+            this.comboBox1StockDataType = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gvListData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "登入ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密碼";
+            // 
+            // textBox1LoginID
+            // 
+            this.textBox1LoginID.Location = new System.Drawing.Point(17, 36);
+            this.textBox1LoginID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1LoginID.Name = "textBox1LoginID";
+            this.textBox1LoginID.Size = new System.Drawing.Size(132, 25);
+            this.textBox1LoginID.TabIndex = 2;
+            // 
+            // textBox2Password
+            // 
+            this.textBox2Password.Location = new System.Drawing.Point(181, 36);
+            this.textBox2Password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2Password.Name = "textBox2Password";
+            this.textBox2Password.PasswordChar = '*';
+            this.textBox2Password.Size = new System.Drawing.Size(132, 25);
+            this.textBox2Password.TabIndex = 3;
+            // 
+            // button1Login
+            // 
+            this.button1Login.Location = new System.Drawing.Point(339, 36);
+            this.button1Login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1Login.Name = "button1Login";
+            this.button1Login.Size = new System.Drawing.Size(100, 29);
+            this.button1Login.TabIndex = 4;
+            this.button1Login.Text = "登入";
+            this.button1Login.UseVisualStyleBackColor = true;
+            this.button1Login.Click += new System.EventHandler(this.button1Login_Click);
+            // 
+            // buttonEnterMonitorLONG_click
+            // 
+            this.buttonEnterMonitorLONG_click.Location = new System.Drawing.Point(20, 89);
+            this.buttonEnterMonitorLONG_click.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonEnterMonitorLONG_click.Name = "buttonEnterMonitorLONG_click";
+            this.buttonEnterMonitorLONG_click.Size = new System.Drawing.Size(100, 29);
+            this.buttonEnterMonitorLONG_click.TabIndex = 7;
+            this.buttonEnterMonitorLONG_click.Text = "繼續";
+            this.buttonEnterMonitorLONG_click.UseVisualStyleBackColor = true;
+            this.buttonEnterMonitorLONG_click.Click += new System.EventHandler(this.buttonEnterMonitorLONG_click_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(557, 39);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(353, 193);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // StockList
+            // 
+            this.StockList.FormattingEnabled = true;
+            this.StockList.ItemHeight = 15;
+            this.StockList.Location = new System.Drawing.Point(557, 301);
+            this.StockList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StockList.Name = "StockList";
+            this.StockList.Size = new System.Drawing.Size(353, 109);
+            this.StockList.TabIndex = 9;
+            // 
+            // gvListData
+            // 
+            this.gvListData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvListData.Location = new System.Drawing.Point(20, 301);
+            this.gvListData.Name = "gvListData";
+            this.gvListData.RowHeadersWidth = 51;
+            this.gvListData.RowTemplate.Height = 27;
+            this.gvListData.Size = new System.Drawing.Size(240, 150);
+            this.gvListData.TabIndex = 10;
+            // 
+            // button1gvSearch
+            // 
+            this.button1gvSearch.Location = new System.Drawing.Point(20, 490);
+            this.button1gvSearch.Name = "button1gvSearch";
+            this.button1gvSearch.Size = new System.Drawing.Size(75, 23);
+            this.button1gvSearch.TabIndex = 11;
+            this.button1gvSearch.Text = "查詢";
+            this.button1gvSearch.UseVisualStyleBackColor = true;
+            // 
+            // textBox1StockData
+            // 
+            this.textBox1StockData.Location = new System.Drawing.Point(20, 457);
+            this.textBox1StockData.Name = "textBox1StockData";
+            this.textBox1StockData.Size = new System.Drawing.Size(100, 25);
+            this.textBox1StockData.TabIndex = 12;
+            // 
+            // comboBox1StockDataType
+            // 
+            this.comboBox1StockDataType.FormattingEnabled = true;
+            this.comboBox1StockDataType.Location = new System.Drawing.Point(139, 457);
+            this.comboBox1StockDataType.Name = "comboBox1StockDataType";
+            this.comboBox1StockDataType.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1StockDataType.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.comboBox1StockDataType);
+            this.Controls.Add(this.textBox1StockData);
+            this.Controls.Add(this.button1gvSearch);
+            this.Controls.Add(this.gvListData);
+            this.Controls.Add(this.StockList);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonEnterMonitorLONG_click);
+            this.Controls.Add(this.button1Login);
+            this.Controls.Add(this.textBox2Password);
+            this.Controls.Add(this.textBox1LoginID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gvListData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1LoginID;
+        private System.Windows.Forms.TextBox textBox2Password;
+        private System.Windows.Forms.Button button1Login;
+        private System.Windows.Forms.Button buttonEnterMonitorLONG_click;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox StockList;
+        private System.Windows.Forms.DataGridView gvListData;
+        private System.Windows.Forms.Button button1gvSearch;
+        private System.Windows.Forms.TextBox textBox1StockData;
+        private System.Windows.Forms.ComboBox comboBox1StockDataType;
+    }
+}
+
---
+++ week4/Form1.cs
@@ -0,0 +1,160 @@
+﻿using SKCOMLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMSample1
+{
+    public partial class Form1 : Form
+    {
+        int m_nCode;
+        SKCenterLib m_pSKCenter;
+        //add com object
+        SKQuoteLib m_SKQuote;
+        void OnLoginMessage(string strUserID, string strStatusCode, string strMessage)
+        {
+            if (strStatusCode == "0")//成功
+            {
+                MessageBox.Show(strUserID + "登入成功!");
+            }
+            else
+            {
+                MessageBox.Show(strUserID + "登入失敗!狀態碼： " + strStatusCode + "錯誤訊息： " + strMessage);
+            }
+        }
+        void m_SKQuoteLib_OnConnection(int nKind, int nCode)
+        {
+            if (nKind == 3001)
+            {
+                richTextBox1.AppendText("連線中(尚未連線完成)" + nKind + "\n");
+            }
+            else if (nKind == 3002)
+            {
+                richTextBox1.AppendText("斷線" + nKind + "\n");
+            }
+            else if (nKind == 3003)
+            {
+                richTextBox1.AppendText("連線完成(下載完商品檔))" + nKind + "\n");
+            }
+            else if (nKind == 3033)
+            {
+                //異常
+                richTextBox1.AppendText("與報價主機發生異常斷線: " + nKind + "\n");
+            }
+            else
+            {
+                richTextBox1.AppendText("連線狀態事件失敗:" + nKind + "\n");
+            }
+        }
+        void SKQuoteLib_RequestStockList(short sMarketNo)
+        {
+            m_nCode = m_SKQuote.SKQuoteLib_RequestStockList(sMarketNo);
+        }
+        void m_SKQuoteLib_OnNotifyCommodityListWithTypeNo(short sMarketNo, string bstrStocListData) 
+        {
+            string strData = "";
+            strData = "[OnNotifyCommodityList]" + bstrStocListData;
+            StockList.Items.Add(strData);
+        }
+        //新增
+        void SKQuoteLib_RequestStocks()
+        {
+            m_nCode = m_SKQuote.SKQuoteLib_ReqStocks(textBox1StockData.Text.Trim());
+        }
+        void SKQuoteLib_CancelRequestStocks()
+        {
+            m_nCode = m_SKQuote.SKQuoteLib_CancelRequestStocks(textBox1StockData.Text.Trim());
+        }
+        void m_SKQuoteLib_OnNotifyQuote(short sMarketNo, string strStockNo)
+        {
+            SKSTOCKLONG pSKStockLONG = new SKSTOCKLONG();
+            m_SKQuote.SKQuoteLib_GetStockByMarketAndNo(sMarketNo, strStockNo, ref pSKStockLONG);
+            OnUpDateDataRow(pSKStockLONG);
+        }
+        void SKQuoteLib_RequestTicks()
+        {
+            m_nCode = m_SKQuote.SKQuoteLib_RequestTicks(ref sPage, textBox1StockData.Text.Trim());
+        }
+        void SKQuoteLib_CancelRequestTicks()
+        {
+            m_nCode = m_SKQuote.SKQuoteLib_CancelRequestTicks(textBox1StockData.Text.Trim());
+        }
+        void OnNotifyBest5LONG()
+        {
+            m_SKQuote.OnNotifyBest5LONG += new _ISKQuoteLibEvents_OnNotifyBest5LONGEventHandler(m_SKQuoteLib_OnNotifyBest5);
+        }
+        void m_SKQuoteLib_OnNotifyBest5(short sMarketNo, string bstrStockNo,
+        int nBestBid1, int nBestBidQty1, int nBestBid2, int nBestBidQty2, int nBestBid3, int nBestBidQty3,
+        int nBestBid4, int nBestBidQty4, int nBestBid5, int nBestBidQty5, int nExtendBid, int nExtendBidQty,
+        int nBestAsk1, int nBestAskQty1, int nBestAsk2, int nBestAskQty2, int nBestAsk3, int nBestAskQty3,
+        int nBestAsk4, int nBestAskQty4, int nBestAsk5, int nBestAskQty5, int nExtendAsk, int nExtendAskQty, int nSimulate)
+        {
+
+        }
+        void OnNotifyTicksLONG()
+        {
+            m_SKQuote.OnNotifyTicksLONG += new _ISKQuoteLibEvents_OnNotifyTicksLONGEventHandler(m_SKQuoteLib_OnNotifyTicks);
+        }
+        void m_SKQuoteLib_OnNotifyTicks(short sMarketNo, string strStockNo, int nPtr, int nDate, int lTimehms, 
+        int lTimemillismicros, int nBid, int nAsk, int nClose, int nQty, int nSimulate)
+        {
+
+        }
+        void OnNotifyHistoryTicksLONG()
+        {
+            m_SKQuote.OnNotifyHistoryTicksLONG += new _ISKQuoteLibEvents_OnNotifyHistoryTicksLONGEventHandler(m_SKQuoteLib_OnNotifyHistoryTicks);
+        }
+        void m_SKQuoteLib_OnNotifyHistoryTicks(short sMarketNo,
+        string strStockNo, int nPtr, int nDate, int lTimehms,
+        int lTimemillismicros, int nBid, int nAsk, int nClose,
+        int nQty, int nSimulate)
+        {
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            m_SKQuote = new SKQuoteLib();
+            m_pSKCenter = new SKCenterLib();
+
+            m_pSKCenter.OnLoginMessage += new _ISKCenterLibEvents_OnLoginMessageEventHandler(this.OnLoginMessage);
+            m_SKQuote.OnConnection += new _ISKQuoteLibEvents_OnConnectionEventHandler(m_SKQuoteLib_OnConnection);
+            m_SKQuote.OnNotifyQuoteLONG += new _ISKQuoteLibEvents_OnNotifyQuoteLONGEventHandler(m_SKQuoteLib_OnNotifyQuote);
+        }
+
+        private void button1Login_Click(object sender, EventArgs e)
+        {
+            int nSeqId = 0;
+            m_nCode = m_pSKCenter.SKCenterLib_SocketLogin(ref nSeqId, textBox1LoginID.Text.Trim().ToUpper(), textBox2Password.Text.Trim());
+            if (m_nCode == 0)
+            {
+                MessageBox.Show(textBox1LoginID.Text.Trim() + " 呼叫登入函式");
+            }
+            else
+            {
+                MessageBox.Show(textBox1LoginID.Text.Trim() + " 呼叫登入函式失敗!錯誤代碼:" + m_nCode);
+            }
+        }
+
+        private void buttonEnterMonitorLONG_click_Click(object sender, EventArgs e)
+        {
+            m_nCode = m_SKQuote.SKQuoteLib_EnterMonitorLONG();
+            if (m_nCode == 0)
+            {
+                richTextBox1.AppendText(textBox1LoginID.Text.Trim() + " 呼叫連線函式成功\n");
+            }
+            else
+            {
+                richTextBox1.AppendText(textBox1LoginID.Text.Trim() + " 呼叫連線函式失敗" + m_nCode + "\n");
+
+            }
+        }
+    }
+}
